0245a1f7a5b265600136f45e638cd9effe901a94
Aysenurgnl <aysenurgunall50@gmail.com>
2021-02-28 13:57:50 +0300

    first commit

---
+++ Homework--3/CustomCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCellTableViewCell.swift
+//  Homework--3
+//
+//  Created by Ayşenur Günal on 18.01.2021.
+//
+
+import UIKit
+
+class CustomCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Homework--3/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  Homework--3
+//
+//  Created by Ayşenur Günal on 18.01.2021.
+//
+
+import Foundation
